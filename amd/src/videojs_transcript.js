df601885e6371d1df2a6b984c8be7dd151e0008d
Matt Porritt <mattp@catalyst-au.net>
2017-07-20 14:25:19 +1000

    update transcript handling so if the video is paused and a user clicks on the transcript, video starts playing again

--- amd/src/videojs_transcript.js
+++ amd/src/videojs_transcript.js
@@ -371,6 +371,7 @@ define(['bc'], function () {
                 (plugin.settings.clickArea === 'timestamp' && clickedClasses.contains(plugin.prefix + '-timestamp')) ||
                 (plugin.settings.clickArea === 'text' && clickedClasses.contains(plugin.prefix + '-text'))) {
                 plugin.player.currentTime(clickedTime);
+                plugin.player.play();
               }
             }
           };
