c5f4f0f5e11f14afa8a07369be87703ab12cf8a8
Matt Porritt <mattp@catalyst-au.net>
2017-08-17 16:32:39 +1000

    add activty icon

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_brightcove';
-$plugin->release = '2017081700';
-$plugin->version = 2017081700;
+$plugin->release = '2017081701';
+$plugin->version = 2017081701;
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_ALPHA;
