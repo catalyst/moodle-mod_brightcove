96e7cf7bf2ec88262211d5acbc0ab4225e9e3aa3
Matt Porritt <mattp@catalyst-au.net>
2017-08-18 15:58:22 +1000

    add webservice boilerplate

---
+++ db/services.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Brightcove Activity web service external functions and service definitions.
+ *
+ * @package    mod_brightcove
+ * @copyright  2017 Matt Porritt <mattp@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// We defined the web service functions to install.
+$functions = array(
+        'mod_brightcove_video_list' => array(
+                'classname'   => 'mod_brightcove_external',
+                'methodname'  => 'video_list',
+                'classpath'   => 'mod/brightcove/externallib.php',
+                'description' => 'Returns available videos via the Brightcove API',
+                'type'        => 'read',
+                'capabilities'  => 'mod/brightcove:addinstance',
+                'ajax' => true
+        ),
+);
---
+++ externallib.php
@@ -0,0 +1,61 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Brightcove Activity Web Service
+ *
+ * @package    mod_brightcove
+ * @copyright  2017 Matt Porritt <mattp@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir . "/externallib.php");
+
+/**
+ * Brightcove Activity Web Service
+ *
+ * @package    mod_brightcove
+ * @copyright  2017 Matt Porritt <mattp@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_brightcove_external extends external_api {
+
+    /**
+     * Returns description of method parameters
+     * @return external_function_parameters
+     */
+    public static function video_list_parameters() {
+
+    }
+
+    /**
+     * Returns available videos
+     *
+     */
+    public static function video_list() {
+
+    }
+
+    /**
+     * Returns description of method result value
+     * @return external_description
+     */
+    public static function video_list_returns() {
+
+    }
+
+}
